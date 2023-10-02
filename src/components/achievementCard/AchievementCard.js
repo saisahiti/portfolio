7d4a84888cda5a9933e95d5ab406a1f05c8280fa
Sai Sahiti <37466512+saisahiti@users.noreply.github.com>
2023-10-02 13:16:07 -0400

    Add files via upload

---
+++ src/components/achievementCard/AchievementCard.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./AchievementCard.scss";
+
+export default function AchievementCard({cardInfo, isDark}) {
+  function openUrlInNewTab(url, name) {
+    if (!url) {
+      console.log(`URL for ${name} not found`);
+      return;
+    }
+    var win = window.open(url, "_blank");
+    win.focus();
+  }
+
+  return (
+    <div className={isDark ? "dark-mode certificate-card" : "certificate-card"}>
+      <div className="certificate-image-div">
+        <img
+          src={cardInfo.image}
+          className="card-image"
+        ></img>
+      </div>
+      <div className="certificate-detail-div">
+        <h5 className={isDark ? "dark-mode card-title" : "card-title"}>
+          {cardInfo.title}
+        </h5>
+        <p className={isDark ? "dark-mode card-subtitle" : "card-subtitle"}>
+          {cardInfo.description}
+        </p>
+      </div>
+    </div>
+  );
+}
